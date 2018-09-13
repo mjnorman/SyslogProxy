ad115bb2f4427d52d4274782e2986142612c9686
Michael Norman <michael_norman@cable.comcast.com>
2018-09-13 12:32:40 -0500

    Create logic for extracting data elements from syslog. Add additional tests for various use cases.

--- src/SyslogProxy/Messages/JsonSyslogMessage.cs
+++ src/SyslogProxy/Messages/JsonSyslogMessage.cs
@@ -1,6 +1,7 @@
 ﻿namespace SyslogProxy.Messages
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Text.RegularExpressions;
 
@@ -9,14 +10,21 @@
     public class JsonSyslogMessage
     {
         private static readonly Regex _DateStampRegex = new Regex(@"\w{3} \w{3}.{4}\d{2}:\d{2}:\d{2}.\d{3}");
+        
         // See https://tools.ietf.org/html/rfc5424#section-6 
-        private static readonly string _rfc5424Regex = @"\<(?<PRI>\d+)\>(?<VERSION>\d{1,2}) (?<TIMESTAMP>(?:\b\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d\.\d{6}[\+,\-]\d\d:\d\d\b|-)) (?<HOSTNAME>(?:[\x21-\x7E]{1,255}|-)){1,255} (?<APPNAME>(?:[\x21-\x7E]{1,48}|-)) (?<PROCID>[\x21-\x7E]{1,128})? (?<MSGID>(?:[\x21-\x7E]{1,32}|-)) (?<SD>(?:\[(?<SDID>[\x21\x23-\x3C\x3E-\x5A\x5C-\x7E]{1,32})(?: (?<SDPARAM>(?<PARAMNAME>[\x21\x23-\x3C\x3E-\x5A\x5C-\x7E]{1,32})=""(?< PARAMVALUE >[^ ""]*)"")) *\]| -)) (?<MSG>.*)"")";
+        private static readonly string _rfc5424Regex = @"\<(?<PRI>\d+)\>(?<VERSION>\d{1,2}) (?<TIMESTAMP>(?:\b\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d\.\d{6}[\+,\-]\d\d:\d\d\b|-)) (?<HOSTNAME>(?:[\x21-\x7E]{1,255}|-)){1,255} (?<APPNAME>(?:[\x21-\x7E]{1,48}|-)) (?<PROCID>[\x21-\x7E]{1,128})? (?<MSGID>(?:[\x21-\x7E]{1,32}|-)) (?<SD>(?:\[(?<SDID>[\x21\x23-\x3C\x3E-\x5A\x5C-\x7E]{1,32})(?: (?<SDPARAM>(?<PARAMNAME>[\x21\x23-\x3C\x3E-\x5A\x5C-\x7E]{1,32})=""(?<PARAMVALUE>[^""]*)""))*\]|-)) (?<MSG>.*)";
+        private static readonly string _sdpairRegex = @"(?<PARAMNAME>[\x21\x23-\x3C\x3E-\x5A\x5C-\x7E]{1,32})=""(?<PARAMVALUE>[^ ""]*)""";
 
         public JsonSyslogMessage(string rawMessage)
         {
+            //init private var
+            StructuredDataElements = new Dictionary<string, string>();
+
             RawMessage = rawMessage;
 
-            ParseRFC5424();
+            //if we can parse full rfc5424 return after
+            if(ParseRFC5424())
+                return;
 
             var splitLine = rawMessage.Split(' ');
             if (splitLine.Length < 4)
@@ -53,20 +61,48 @@
         private bool ParseRFC5424()
         {
             var regex = new Regex(_rfc5424Regex);
-            var match = Regex.Match(RawMessage, _rfc5424Regex, RegexOptions.IgnoreCase);
+            var match = Regex.Match(RawMessage, _rfc5424Regex);
 
             //check for rfc5424, return to original match if no match
             if (!match.Success)
+            {
+                //ensure Invalid is set before return
+                Invalid = true;
                 return false;
+            }
 
-            Timestamp = match.Groups["TIMESTAMP"]?.Value;
-            Hostname = match.Groups["HOSTNAME"]?.Value;
-            ApplicationName = match.Groups["APPNAME"]?.Value;
-            Message = match.Groups["MSG"]?.Value;
+            //extract values from syslog
+            Timestamp = match.Groups["TIMESTAMP"].Value;
+            Hostname = match.Groups["HOSTNAME"].Value;
+            ApplicationName = match.Groups["APPNAME"].Value;
+            Message = match.Groups["MSG"].Value;
+            MessageID = match.Groups["MSGID"].Value;
+            StructuredData = match.Groups["SD"].Value;
+            ProcID = match.Groups["PROCID"].Value;
+
+            if (StructuredData != "-")
+            {
+                ParseStructuredData(StructuredData);
+            }
+
+            //ensure Invald = false
+            Invalid = false;
 
             return true;
         }
 
+        private void ParseStructuredData(string sd)
+        {
+            var sdmatches = Regex.Matches(sd, _sdpairRegex);
+            if (sdmatches.Count > 0)
+            {
+                foreach (Match sdmatch in sdmatches)
+                {
+                    StructuredDataElements.Add(sdmatch.Groups["PARAMNAME"].Value, sdmatch.Groups["PARAMVALUE"].Value);
+                }
+            }
+
+        }
 
         public bool Invalid { get; private set; }
 
@@ -83,6 +119,14 @@
         public string ApplicationName { get; set; }
         
         public string Message { get; set; }
+
+        public string MessageID { get; set; }
+
+        public string StructuredData { get; set; }
+
+        public string ProcID { get; private set; }
+
+        public Dictionary<string, string> StructuredDataElements { get; set; }
 
         public override string ToString()
         {
--- test/SyslogProxy.UnitTests/SyslogJsonTest.cs
+++ test/SyslogProxy.UnitTests/SyslogJsonTest.cs
@@ -1,9 +1,13 @@
 ﻿//<78>2015-02-10T19:26:01.889893+00:00 mongo-monitor CROND  (ec2-user) CMD (/home/ec2-user/bin/run_node_metrics)
 
+
 namespace SyslogProxy.UnitTests
 {
+    using FluentAssertions;
     using global::SyslogProxy.Messages;
-
+    using System;
+    using System.Diagnostics;
+    using System.Text.RegularExpressions;
     using Xunit;
 
     public class SyslogJsonTest
@@ -15,5 +19,42 @@ namespace SyslogProxy.UnitTests
 
             //Program.WriteToSeq(thing).Wait();
         }
+
+        [Fact]
+        public void CanParseRFC5424WithStructuredData()
+        {
+            JsonSyslogMessage syslog = new JsonSyslogMessage(@"164 <14>1 2018-09-07T16:15:38.477531+00:00 NCO.MOM.mom fea3e98a-f9da-4705-91f0-80ef34b38b31 [0] - [gauge@47450 name=""disk_quota"" value=""1.073741824e+09"" unit=""bytes""] ");
+            //JsonSyslogMessage thing = new JsonSyslogMessage(@"[gauge@47450 name=""disk_quota"" test=""testing""]");
+
+            syslog.Invalid.Should().BeFalse();
+            syslog.Timestamp.Should().Be("2018-09-07T16:15:38.477531+00:00");
+            syslog.Hostname.Should().Be("NCO.MOM.mom");
+            syslog.ApplicationName.Should().Be("fea3e98a-f9da-4705-91f0-80ef34b38b31");
+            syslog.StructuredData.Should().Be(@"[gauge@47450 name=""disk_quota"" value=""1.073741824e+09"" unit=""bytes""]");
+            syslog.StructuredDataElements.Count.Should().Be(3);
+            syslog.StructuredDataElements["name"].Should().Be("disk_quota");
+            syslog.StructuredDataElements["value"].Should().Be("1.073741824e+09");
+            syslog.StructuredDataElements["unit"].Should().Be("bytes");
+            syslog.Message.Should().BeEmpty();
+            syslog.ProcID.Should().Be("[0]");
+        }
+
+        [Fact]
+        public void CanParseRFC5424WithoutStructuredData()
+        {
+            JsonSyslogMessage syslog = new JsonSyslogMessage(@"164 <14>1 2018-09-13T17:02:34.936791+00:00 hostname app-name [APP/PROC/WEB/0] - - message is here");
+            //JsonSyslogMessage thing = new JsonSyslogMessage(@"[gauge@47450 name=""disk_quota"" test=""testing""]");
+
+            syslog.Invalid.Should().BeFalse();
+            syslog.Timestamp.Should().Be("2018-09-13T17:02:34.936791+00:00");
+            syslog.Hostname.Should().Be("hostname");
+            syslog.ApplicationName.Should().Be("app-name");
+            syslog.StructuredData.Should().Be(@"-");
+            syslog.ProcID.Should().Be("[APP/PROC/WEB/0]");
+            syslog.Message.Should().Be("message is here");
+            syslog.MessageID.Should().Be("-");
+
+        }
+
     }
 }
